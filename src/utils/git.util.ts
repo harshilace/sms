92f7ddbe624891970f6a24283476b702093be2aa
harshil Solanki <harshilsolanki.ace@gmail.com>
2024-04-16 13:13:55 +0530

    Git utill execute once app bootsraped

--- src/utils/git.util.ts
+++ src/utils/git.util.ts
@@ -1,17 +1,17 @@
-// git.service.ts
-
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import * as fs from 'fs-extra';
 import * as childProcess from 'child_process';
 
 @Injectable()
-export class GitUtil {
-    async initializeProjects(parentFolderPath: string, projectsFolderName: string): Promise<void> {
-        const projectsFolderPath = `${parentFolderPath}/${projectsFolderName}`;
+export class GitUtil implements OnApplicationBootstrap {
 
+    async onApplicationBootstrap() {
         try {
-            const projects = await fs.readdir(projectsFolderPath);
-            console.log(['projects', projects]);
+            console.log(['START GIT PROCESS']);
+
+            const path = `/home/harshil/Desktop/Harshil Refrences/Rajkot-Code backup/animesh`;
+            const folders = await fs.readdir(path);
+            console.log(['Folders', folders]);
 
             let names = {
                 '3837': 'https://harshilsolanki:glpat-P9Ngmy716_qjzxYty3ss@git.zerozone.com/ace-infoway/animesh-santoki/3837.git',
@@ -20,62 +20,42 @@ export class GitUtil {
                 '3971': 'https://harshilsolanki:glpat-P9Ngmy716_qjzxYty3ss@git.zerozone.com/ace-infoway/animesh-santoki/3971.git',
             }
 
-            for (const project of projects) {
-                const projectFolderPath = `${projectsFolderPath}/${project}`;
-                // console.log(['project', project]);
-                // console.log(['projectFolderPath', projectFolderPath]);
-                let url = names[project];
+            for (const folder of folders) {
+                const projectFolderPath = `${path}/${folder}`;
+                let url = names[folder];
                 console.log(['URL:' + url]);
 
                 if ((await fs.stat(projectFolderPath)).isDirectory() && url) {
-                    // Navigate into the project folder
-                    process.chdir(projectFolderPath);
-
-                    // Run git commands
+                    
+                    process.chdir(projectFolderPath);   // Navigate into the project folder
                     console.log(['Test-1']);
                     childProcess.execSync('git init');
                     console.log(['Test-2']);
-                    // childProcess.execSync(`git remote add origin https://git.zerozone.com/ace-infoway/animesh-santoki/${project}.git`);
-                    // childProcess.execSync(`git remote add origin ${names[project]}`);
                     childProcess.execSync(`git config remote.origin.url ${url}`);
                     console.log(['Test-3']);
                     childProcess.execSync('git add .');
                     console.log(['Test-4']);
                     const gitStatus = childProcess.execSync('git status --porcelain').toString().trim();
                     console.log(['Test-5']);
+                    
                     if (gitStatus !== '') {
                         console.log(['Test-5.1']);
                         childProcess.execSync('git commit -m "Project Initialized"');
                         console.log(['Test-5.2']);
                         childProcess.execSync('git push --set-upstream origin master');
                     } else {
-                        console.log(`No changes to commit in ${project}. Skipping commit.`);
+                        console.log(`No changes to commit in ${folder}. Skipping commit.`);
                     }
                     console.log(['Test-6']);
 
                     // Navigate back to the projects folder
-                    process.chdir(projectsFolderPath);
+                    process.chdir(path);
                 }
             }
+
+            console.log(['END GIT PROCESS']);
         } catch (error) {
             throw new Error(`Error initializing projects: ${error.message}`);
         }
     }
 }
-
-async function executeGitUtil() {
-    try {
-        console.log(['START GIT PROCESS']);
-        const gitUtil = new GitUtil();
-        await gitUtil.initializeProjects('/home/harshil/Desktop/Harshil Refrences/Rajkot-Code backup', 'animesh')
-            .then(() => console.log("Migrations executed successfully in all databases"))
-            .catch(error => console.error("Error executing migrations:", error));
-
-
-        console.log(['END GIT PROCESS']);
-    } catch (error) {
-        console.error("Migration process failed:", error);
-    }
-}
-
-executeGitUtil();
